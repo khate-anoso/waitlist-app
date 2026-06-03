2980ea3fd8492830a696effb6b6412594e27ca8e
Khate <khate@2degreesconnections.com>
2026-06-03 18:23:33 -0500

    fix double headers bug

--- app/api/monday/route.ts
+++ app/api/monday/route.ts
@@ -56,10 +56,10 @@ export async function POST(req: Request) {
   const res = await fetch("https://api.monday.com/v2", {
     method: "POST",
     headers: {
-      Authorization: "eAuthorization: "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjY2NjY5NTAzNSwiYWFpIjoxMSwidWlkIjo5NTQwMzYxMiwiaWFkIjoiMjAyNi0wNi0wM1QyMDoyODoyOS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MzAzNDM5MTMsInJnbiI6InVzZTEifQ._VZ7_sBN34oUXmumJtn-EWIe6yhVPFqNZs2K9dWDynk",
-", 
-      "Content-Type": "application/json"
-    },
+  Authorization: process.env.MONDAY_API_KEY!,
+  "Content-Type": "application/json"
+},
+
     body: JSON.stringify({ query })
   });
 
