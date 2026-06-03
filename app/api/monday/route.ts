6ba8d99ddce77317eef31854a37d42b0be6c221b
Khate <khate@2degreesconnections.com>
2026-06-03 17:34:36 -0500

    add POST for monday

--- app/api/monday/route.ts
+++ app/api/monday/route.ts
@@ -29,3 +29,34 @@ export async function GET() {
 
   return Response.json(data);
 }
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const { name, date } = body;
+
+  const query = `
+    mutation {
+      create_item(
+        board_id: 18412930770,
+        item_name: "${name}",
+        column_values: "{\"date_mm3a5hvm\": {\"date\": \"${date}\"}}"
+      ) {
+        id
+      }
+    }
+  `;
+
+  const res = await fetch("https://api.monday.com/v2", {
+    method: "POST",
+    headers: {
+      Authorization: "eyJhbGciOiJIUzI1NiJ9...", // SAME TOKEN MO
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ query })
+  });
+
+  const data = await res.json();
+
+  return Response.json(data);
+}
